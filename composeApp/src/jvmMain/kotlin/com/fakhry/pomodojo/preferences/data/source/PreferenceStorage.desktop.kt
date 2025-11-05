f828ab28930f5f28cccf732e2c6f740e9d5da7a1
Fakhry <fakhrymubarak@gmail.com>
2025-11-05 10:08:48 +0800

    feat: Implement singleton `DataStore` for preferences

---
+++ composeApp/src/jvmMain/kotlin/com/fakhry/pomodojo/preferences/data/source/PreferenceStorage.desktop.kt
@@ -0,0 +1,32 @@
+package com.fakhry.pomodojo.preferences.data.source
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import okio.Path
+import okio.Path.Companion.toPath
+import java.io.File
+
+internal actual fun provideDataStore(): DataStore<Preferences> =
+    DesktopPreferencesDataStoreProvider.dataStore
+
+private object DesktopPreferencesDataStoreProvider {
+    private val scope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
+
+    val dataStore: DataStore<Preferences> by lazy {
+        val home = System.getProperty("user.home") ?: "."
+        val directory = File(home, ".pomodojo").apply {
+            if (!exists()) {
+                mkdirs()
+            }
+        }
+        val path: Path = File(directory, PREFERENCES_FILE_NAME).absolutePath.toPath()
+        PreferenceDataStoreFactory.createWithPath(
+            scope = scope,
+            produceFile = { path },
+        )
+    }
+}
