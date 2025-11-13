0eb22748641df95b479abefd2a57b42547ec1039
Fakhry <fakhrymubarak@gmail.com>
2025-11-13 20:38:17 +0800

    feat: Implement session completion notification

--- composeApp/src/androidMain/kotlin/com/fakhry/pomodojo/focus/domain/model/NotificationSummary.kt
+++ composeApp/src/androidMain/kotlin/com/fakhry/pomodojo/focus/domain/model/NotificationSummary.kt
@@ -1,11 +1,12 @@
 package com.fakhry.pomodojo.focus.domain.model
 
 internal data class NotificationSummary(
-    val sessionId: String,
-    val title: String,
-    val timerText: String,
-    val segmentProgressPercent: Int,
-    val isPaused: Boolean,
+    val sessionId: String = "",
+    val title: String = "",
+    val timerText: String = "",
+    val segmentProgressPercent: Int = 0,
+    val isPaused: Boolean = false,
     val finishTimeMillis: Long,
-    val quote: String,
+    val quote: String = "",
+    val isAllSegmentsCompleted: Boolean = false,
 )
