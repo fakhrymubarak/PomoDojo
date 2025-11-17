cd6642cfe666f905439ee9931c56ed849330d1e5
Fakhry <fakhrymubarak@gmail.com>
2025-11-17 11:48:11 +0800

    feat: Add icons to app theme display names

--- composeApp/src/commonMain/kotlin/com/fakhry/pomodojo/preferences/domain/model/AppTheme.kt
+++ composeApp/src/commonMain/kotlin/com/fakhry/pomodojo/preferences/domain/model/AppTheme.kt
@@ -4,8 +4,8 @@ package com.fakhry.pomodojo.preferences.domain.model
  * Supported application appearance themes.
  */
 enum class AppTheme(val storageValue: String, val displayName: String) {
-    DARK(storageValue = "dark", displayName = "Dark"),
-    LIGHT(storageValue = "light", displayName = "Light"),
+    DARK(storageValue = "dark", displayName = "🌘Dark"),
+    LIGHT(storageValue = "light", displayName = "☀️Light"),
     ;
 
     companion object {
